ef2ea3e07fc26111a9b0d0bd62adb19846a822fe
rsbastos <renedossantosbastos@gmail.com>
2023-10-18 20:51:46 -0300

    page 2 content finished

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,9 +7,9 @@ export const dataFake = [
     },
     {
         "id":"2",       
-        "title":"lançado novo álbum",
-        "description":"bla bla bla",
-        "photoCover":"https://static.todamateria.com.br/upload/ro/ck/rock-a-origem-e-historia-do-rock-and-roll-og.jpg"
+        "title":"Ozzy Osbourne participa do Famous Monsters Festival com sua família",
+        "description":"A Famous Monsters, revista fundada em 1958, comemorou seu 65º aniversário no último final de semana com um evento de três dias que contou com a participação de artistas como Ozzy, Sharon, Jack e Kelly Osbourne, Scott Ian e Frank Bello, do Anthrax, a lenda do terror Tom Savini e o cofundador da Famous Monsters, James Warren. O evento foi realizado no Valley Forge Casino Resort na Pensilvânia, Estados Unidos. De acordo com o site do festival, Ozzy apareceu por lá apenas na sexta-feira (13) e no domingo (15), enquanto sua esposa e empresária Sharon, e seus filhos Jack e Kelly participaram dos três dias do evento. O Famous Monsters Festival ofereceu sessões fotográficas exclusivas, sessões de perguntas e respostas, painéis de discussão, aulas e apresentações durante o final de semana. Famous Monsters é a única revista original de fãs de ficção científica, fantasia, terror e cultura pop que permanece em circulação até hoje. Atualmente, a revista tem distribuição mundial e a empresa cresceu para incluir quadrinhos, roupas, uma linha exclusiva de chocolates, guitarras licenciadas, entre outros produtos. Veja a matéria original em: 'https://www.rockbizz.com.br/ozzy-osbourne-participa-do-famous-monsters-festival-com-sua-familia-confira/'",
+        "photoCover":"https://www.rockbizz.com.br/wp-content/uploads/2022/11/ozzy-osbourne-divulgacao-2022-830x400.jpg"
     },
     {
         "id":"3",      
